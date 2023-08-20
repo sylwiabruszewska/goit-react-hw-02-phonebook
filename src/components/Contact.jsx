5e80678486fe930d6c4da439c21598593d1c975f
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-20 19:57:36 +0200

    add Contact component, styles, propTypes

---
+++ src/components/Contact.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Button from './Button';
+
+const StyledItem = styled.li`
+  font-size: 16px;
+  display: flex;
+  gap: 20px;
+`;
+
+export default function Contact({ name, number, handleDelete }) {
+  return (
+    <StyledItem>
+      <span>{name}</span>
+      <span>{number}</span>
+      <Button handler={handleDelete}></Button>
+    </StyledItem>
+  );
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+};
