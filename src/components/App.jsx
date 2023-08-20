9f2fca4562eee721d12fd473ae56c6cd35f8d438
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-20 20:09:57 +0200

    make App a class component, add contacts for state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
+import { Component } from 'react';
+
 import Section from './Section';
 import ContactForm from './ContactForm';
+import ContactList from './ContactList';
 
 export const StyledApp = styled.div`
   background-color: #fff;
@@ -12,25 +15,35 @@ export const StyledApp = styled.div`
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 `;
 
-export const App = () => {
-  return (
-    <StyledApp>
-      <Section title="Phonebook">
-        <ContactForm />
-      </Section>
-      <Section title="Contacts">
-        <div>
-          <label>
-            Find contacts by name
-            <input type="text" />
-          </label>
-        </div>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-      </Section>
-    </StyledApp>
-  );
-};
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+  };
+
+  render() {
+    const contacts = this.state.contacts;
+
+    return (
+      <StyledApp>
+        <Section title="Phonebook">
+          <ContactForm />
+        </Section>
+        <Section title="Contacts">
+          <div>
+            <label>
+              Find contacts by name
+              <input type="text" />
+            </label>
+          </div>
+          <ContactList contacts={contacts}></ContactList>
+        </Section>
+      </StyledApp>
+    );
+  }
+}
