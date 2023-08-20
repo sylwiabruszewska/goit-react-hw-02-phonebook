c71c53e2b64e0b12262fbeb4494c521d344008d9
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-20 17:14:42 +0200

    add basic structure for App component and add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
+import styled from 'styled-components';
+
+export const StyledApp = styled.div`
+  background-color: #fff;
+  margin: 20px 0;
+  padding: 20px;
+  border-radius: 10px;
+  color: #313131;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <StyledApp>
+      <section>
+        <h2>Phonebook</h2>
+        <form action="">
+          <label>
+            Name
+            <input type="text" />
+          </label>
+          <button>Add contact</button>
+        </form>
+      </section>
+      <section>
+        <h2>Contacts</h2>
+        <div>
+          <label>
+            Find contacts by name
+            <input type="text" />
+          </label>
+        </div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </section>
+    </StyledApp>
   );
 };
