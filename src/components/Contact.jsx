103efe41fcd1c3c51c92d032442b3645385931bf
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-21 03:03:10 +0200

    add icons for list items, update Contact component styles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,44 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faPhone } from '@fortawesome/free-solid-svg-icons';
+
 import Button from './Button';
 
 const StyledItem = styled.li`
   font-size: 16px;
   display: flex;
-  gap: 20px;
+  gap: 10px;
+  margin-bottom: 10px;
+  align-items: center;
+
+  & div {
+    width: 50%;
+  }
+`;
+
+const StyledBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  margin-right: 10px;
 `;
 
 export default function Contact({ id, name, number, handleDelete }) {
   return (
     <StyledItem>
-      <span>{name}</span>
-      <span>{number}</span>
+      <StyledBox>
+        <StyledIcon icon={faUser} style={{ color: '#71a3ff' }} />
+        <span>{name}</span>
+      </StyledBox>
+
+      <StyledBox>
+        <StyledIcon icon={faPhone} style={{ color: '#71a3ff' }} />
+        <span>{number}</span>
+      </StyledBox>
       <Button type="button" handler={() => handleDelete(id)}>
         Delete
       </Button>
