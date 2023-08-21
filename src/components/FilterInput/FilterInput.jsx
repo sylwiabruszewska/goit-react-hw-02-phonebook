f045536628fc561a62a8ea736f9ebbceb14fccdc
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-21 20:24:35 +0200

    add label for FilterInput

--- src/components/FilterInput/FilterInput.jsx
+++ src/components/FilterInput/FilterInput.jsx
@@ -3,15 +3,18 @@ import { StyledFilterInput } from './FilterInput.styled';
 
 export default function FilterInput({ value, onChange }) {
   return (
-    <StyledFilterInput
-      type="text"
-      name="filter"
-      pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-      title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-      placeholder="Search by name"
-      value={value}
-      onChange={onChange}
-    />
+    <label>
+      Find contacts by name
+      <StyledFilterInput
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        placeholder="Search by name"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
   );
 }
 
