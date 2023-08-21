c0b168b2e97ec520873e44a11d95096afc46a689
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-21 19:32:00 +0200

    update Input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,10 +2,17 @@ import PropTypes from 'prop-types';
 import { StyledInput } from './Input.styled';
 
 export default function Input(props) {
-  const { type, name, pattern, title } = props;
+  const { type, name, pattern, title, placeholder, required } = props;
 
   return (
-    <StyledInput type={type} name={name} pattern={pattern} title={title} />
+    <StyledInput
+      type={type}
+      name={name}
+      title={title}
+      placeholder={placeholder}
+      required={required}
+      pattern={pattern}
+    />
   );
 }
 
@@ -14,4 +21,6 @@ Input.propTypes = {
   name: PropTypes.string.isRequired,
   pattern: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  required: PropTypes.bool,
 };
